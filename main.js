03c509a6751fd314e4cbdce836f7ffaa5b851ad4
Lígia Andrade <andradeligiamp@gmail.com>
2023-11-13 17:25:19 -0300

    Commit Inicial

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    $('headder button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit',function(e){
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereço-imagem-nova').val();
+        const novoItem = $('<li  style = "display:none"></li>');
+        $(`<img src= "${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href ="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamnaho rela>
+                    Ver imagem em tamnaho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereço-imagem-nova').val('')
+
+    
+    })
+    
+})
+
+
+
